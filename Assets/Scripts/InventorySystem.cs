97d03106b389d8addd49ef44a3d709ccb739f764
Kathy <ivonnekatherine5@gmail.com>
2023-05-03 14:14:23 +0200

    Inventory

---
+++ Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public static InventorySystem instance {get; set;}
+
+    public GameObject InventoryScreenUI;
+    public bool isOpen;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        isOpen = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && !isOpen)
+        {
+            Debug.Log("i esta pulsada");
+            InventoryScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && isOpen)
+        {
+            InventoryScreenUI.SetActive
+        }
+    }
+}
