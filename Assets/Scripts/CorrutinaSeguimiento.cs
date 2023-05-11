b78083e56512492a4dd9d490dc0ddef6ccde3fd3
Kathy <ivonnekatherine5@gmail.com>
2023-05-11 10:53:52 +0200

    CORRUTINAS

---
+++ Assets/Scripts/CorrutinaSeguimiento.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrutinaSeguimiento : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public Animator aiAnim;
+    int randNum;
+    public Transform playerTrans, aiTrans, randDest1, randDest2, randDest3, randDest4, randDest5, randDest6, randDest7, randDest8;
+    public bool walking, chasing, idle;
+    public Vector3 dest;
+    public Collider lookerTrigger;
+    public float chaseTime;
+
+    private void Start()
+    {
+        walking = true;
+        randNum = Random.Range(0, 8);
+        aiAnim.SetTrigger("walk");
+
+        if(randNum == 0)
+        {
+            dest = randDest1.position;
+        }
+
+        if(randNum == 1)
+        {
+            dest = randDest2.position;
+        }
+
+        if (randNum == 2)
+        {
+            dest = randDest3.position;
+        }
+
+        if (randNum == 3)
+        {
+            dest = randDest4.position;
+        }
+
+        if (randNum == 4)
+        {
+            dest = randDest5.position;
+        }
+
+        if (randNum == 5)
+        {
+            dest = randDest6.position;
+        }
+
+        if (randNum == 6)
+        {
+            dest = randDest7.position;
+        }
+
+        if (randNum == 7)
+        {
+            dest = randDest8.position;
+        }
+
+    }
+
+    private void Update()
+    {
+        if(walking == true)
+        {
+            ai.destination = dest;
+            ai.speed = 3;
+        }
+
+        if(chasing == true)
+        {
+            dest = playerTrans.position;
+            ai.destination = dest;
+            ai.speed = 6;
+        }
+
+        if(idle == true)
+        {
+            ai.speed = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            chasing = true;
+            walking = false;
+            idle = false;
+            ai.ResetTrigger("idle");
+            ai.ResetTrigger("walk");
+            ai.SetTrigger("run");
+        }
+    }
+}
