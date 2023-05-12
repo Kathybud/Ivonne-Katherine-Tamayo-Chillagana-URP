93a1e59e22c8d65fcfc574000f976d68f6018569
Kathy <ivonnekatherine5@gmail.com>
2023-05-12 16:57:48 +0200

    Corrutina de mierda

--- Assets/Scripts/CorrutinaSeguimiento.cs
+++ Assets/Scripts/CorrutinaSeguimiento.cs
@@ -7,50 +7,64 @@ public class CorrutinaSeguimiento : MonoBehaviour
 {
     public NavMeshAgent ai;
     public Animator aiAnim;
+
     int randNum;
+
     public Transform playerTrans, aiTrans, randDest1, randDest2, randDest3, randDest4, randDest5, randDest6, randDest7, randDest8;
     public bool walking, chasing, idle;
+
     Vector3 dest;
+
     public float chaseTime, idleTime;
 
     void Start()
     {
         walking = true;
         randNum = Random.Range(0, 8);
+
         aiAnim.SetTrigger("walk");
+
         if (randNum == 0)
         {
             dest = randDest1.position;
         }
+
         if (randNum == 1)
         {
             dest = randDest2.position;
         }
+
         if (randNum == 2)
         {
             dest = randDest3.position;
         }
+
         if (randNum == 3)
         {
             dest = randDest4.position;
         }
+
         if (randNum == 4)
         {
             dest = randDest5.position;
         }
+
         if (randNum == 5)
         {
             dest = randDest6.position;
         }
+
         if (randNum == 6)
         {
             dest = randDest7.position;
         }
+
         if (randNum == 7)
         {
             dest = randDest8.position;
         }
     }
+
     void Update()
     {
         if (walking == true)
@@ -58,17 +72,20 @@ public class CorrutinaSeguimiento : MonoBehaviour
             ai.destination = dest;
             ai.speed = 3;
         }
+
         if (chasing == true)
         {
             dest = playerTrans.position;
             ai.destination = dest;
             ai.speed = 6;
         }
+
         if (idle == true)
         {
             ai.speed = 0;
         }
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -76,13 +93,16 @@ public class CorrutinaSeguimiento : MonoBehaviour
             chasing = true;
             walking = false;
             idle = false;
+
             aiAnim.ResetTrigger("idle");
             aiAnim.ResetTrigger("walk");
             aiAnim.SetTrigger("run");
+
             StopCoroutine("nextDest");
             StopCoroutine("chase");
             StartCoroutine("chase");
         }
+
         if (other.CompareTag("destination"))
         {
             if (chasing == false)
@@ -95,84 +115,108 @@ public class CorrutinaSeguimiento : MonoBehaviour
             }
         }
     }
+
     IEnumerator nextDest()
     {
         yield return new WaitForSeconds(idleTime);
+
         idle = false;
         walking = true;
+
         aiAnim.ResetTrigger("idle");
         aiAnim.SetTrigger("walk");
+
         randNum = Random.Range(0, 8);
+
         if (randNum == 0)
         {
             dest = randDest1.position;
         }
+
         if (randNum == 1)
         {
             dest = randDest2.position;
         }
+
         if (randNum == 2)
         {
             dest = randDest3.position;
         }
+
         if (randNum == 3)
         {
             dest = randDest4.position;
         }
+
         if (randNum == 4)
         {
             dest = randDest5.position;
         }
+
         if (randNum == 5)
         {
             dest = randDest6.position;
         }
+
         if (randNum == 6)
         {
             dest = randDest7.position;
         }
+
         if (randNum == 7)
         {
             dest = randDest8.position;
         }
     }
+
     IEnumerator chase()
     {
         yield return new WaitForSeconds(chaseTime);
+
         chasing = false;
         walking = true;
+
         aiAnim.ResetTrigger("idle");
         aiAnim.ResetTrigger("run");
         aiAnim.SetTrigger("walk");
+
         randNum = Random.Range(0, 8);
+
         if (randNum == 0)
         {
             dest = randDest1.position;
         }
+
         if (randNum == 1)
         {
             dest = randDest2.position;
         }
+
         if (randNum == 2)
         {
             dest = randDest3.position;
         }
+
         if (randNum == 3)
         {
             dest = randDest4.position;
         }
+
         if (randNum == 4)
         {
             dest = randDest5.position;
         }
+
         if (randNum == 5)
         {
             dest = randDest6.position;
         }
+
         if (randNum == 6)
         {
             dest = randDest7.position;
         }
+
         if (randNum == 7)
         {
             dest = randDest8.position;
